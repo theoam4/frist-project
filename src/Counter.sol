fea39f4613c4e94af18abf3d0dcc9e54d8e5c0ee
theoam4 <theoam4@outlook.com>
2026-01-23 20:05:05 -0300

    add gerenciamento de número e eventos

--- src/Counter.sol
+++ src/Counter.sol
@@ -3,12 +3,32 @@ pragma solidity ^0.8.13;
 
 contract Counter {
     uint256 public number;
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
 
     function setNumber(uint256 newNumber) public {
+        require(msg.sender == owner, "Only owner can set the number");
         number = newNumber;
+        emit NumberChanged(newNumber, msg.sender);
     }
 
     function increment() public {
         number++;
     }
+
+    function decrement() public {
+        number--;
+    }
+
+    function getNumber() public view returns (uint256) {
+        return number;
+    }
+
+    event NumberChanged(
+        uint256 indexed newNumber,
+        address updatedBy
+    );
 }
